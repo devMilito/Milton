b226fadbbc636c88243397f86571d96feb6fdeba
MILITAO <lucas.militao@mblabs.com.br>
2024-03-20 17:09:36 -0300

    Add project files.

---
+++ Controllers/Usuario/UsuarioController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VinciPartern.Aplicacao.Comando.Usuario;
+
+namespace VinciPartern.Api.Controllers.Usuario
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        [HttpGet]
+        [Route("/obterUsuarioPorId")]
+        public IActionResult ObterUsuarioPorId(ConsultaObterUsuarioPorId consulta)
+        {
+            return Ok(consulta);
+        }
+
+        [HttpPost]
+        [Route("/incluir")]
+        public IActionResult Incluir(ComandoIncluir comando)
+        {
+            return Ok(comando);
+        }
+    }
+}
