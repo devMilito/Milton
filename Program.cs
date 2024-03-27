c8e8e5c2321e68bd03eefca4ad82b44e0dc778fe
MILITAO <lucas.militao@mblabs.com.br>
2024-03-27 16:30:25 -0300

    fluent validation

--- Program.cs
+++ Program.cs
@@ -1,15 +1,20 @@
-var builder = WebApplication.CreateBuilder(args);
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using VinciPartern.Aplicacao.Comando.Usuario;
+using VinciPartern.Entidade;
 
-// Add services to the container.
+WebApplicationBuilder? builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddSingleton<DbContexto>();
+builder.Services.AddDbContext<DbContexto>();
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddFluentValidationAutoValidation();
+builder.Services.AddValidatorsFromAssemblyContaining<ComandoIncluirValidacao>();
 
-var app = builder.Build();
+WebApplication? app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -17,9 +22,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
